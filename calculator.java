a9b56e1135e4675597aa1f9af80b1cbdca61e6f9
imjy2 <imjy2@192.168.35.106>
2022-10-26 23:53:56 +0900

    기능추가

--- calculator.java
+++ calculator.java
@@ -1,7 +1,15 @@
 //계산기
+class calculator{
 public static int add(int x, int y){
 	return x+y;
 }
 public static int substract(int x, int y){
 	return x-y;
+}
+public static double divide(int x, int y) {
+	return (double)x/y;
+}
+public static void say_hi() {
+	System.out.println("hi");
+}
 }
